720f0743729dc8d98d96783119f793fc3a2a5538
solar <nathf1005@naver.com>
2025-02-11 01:34:15 +0900

    feat: 로그인 및 회원가입 링크에 라우터 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
+"use client";
+import { useRouter } from "next/navigation";
 import StickyHeadTable from "./_components/ReactTable";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <div className="mx-5">
       <div className="mt-10" />
       <div className="flex justify-between items-center">
         <p className="text-4xl">홈페이지</p>
         <div className="flex gap-5">
-          <p className="text-sm">로그인</p>
-          <p className="text-sm">회원가입</p>
+          <p className="text-sm" onClick={() => router.push("/login")}>
+            로그인
+          </p>
+          <p className="text-sm" onClick={() => router.push("/register")}>
+            회원가입
+          </p>
         </div>
       </div>
       <div className="mt-16" />

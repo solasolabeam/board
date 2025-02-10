692285fb1b4e212be3a5221a61660a7423f9a976
solar <nathf1005@naver.com>
2025-02-10 22:27:02 +0900

    feat: 홈 페이지에 StickyHeadTable 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+import StickyHeadTable from "./_components/ReactTable";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="mx-5">
+      <div className="mt-10" />
+      <div className="flex justify-between items-center">
+        <p className="text-4xl">홈페이지</p>
+        <div className="flex gap-5">
+          <p>로그인</p>
+          <p>회원가입</p>
+        </div>
+      </div>
+      <div className="mt-28" />
+      <div>
+        <p className="text-3xl text-center mb-24">게시판</p>
+        <StickyHeadTable />
+      </div>
+    </div>
+  );
 }

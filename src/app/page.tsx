49568963d60b54254b065856eab1e9e822abed9f
solar <nathf1005@naver.com>
2025-02-13 22:27:03 +0900

    feat: 글 작성 버튼 추가 및 라우터 기능 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 "use client";
 import Header from "./(components)/Header";
 import StickyHeadTable from "./(components)/ReactTable";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div className="mx-5">
       <div className="mt-10" />
@@ -10,6 +13,14 @@ export default function Home() {
       <div className="mt-16" />
       <div>
         <p className="text-3xl text-center mb-16">게시판</p>
+        <div className="flex justify-end mb-4">
+          <button
+            className="bg-blue-500 text-white px-4 py-3 rounded-md text-xs"
+            onClick={() => router.push("/write")}
+          >
+            글 작성
+          </button>
+        </div>
         <StickyHeadTable />
       </div>
     </div>

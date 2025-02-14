c45077a94d4be07b9487d3f86e06d7fa7607e9ee
solar <nathf1005@naver.com>
2025-02-15 03:03:17 +0900

    feat: FontAwesome 스타일 시트 추가 및 자동 CSS 추가 비활성화

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,4 +1,7 @@
 "use client";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 import { useEffect, useState } from "react";
 import Header from "../(components)/Header";
 import {

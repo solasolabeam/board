637627ae1c2ec3645e2f265ad403a17c924588dc
solar <nathf1005@naver.com>
2025-02-16 23:06:55 +0900

    feat: 로그인 페이지로 리디렉션 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,6 @@
-"use client";
-import Header from "./(components)/Header";
-import StickyHeadTable from "./(components)/ReactTable";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="mx-auto max-w-[1600px] px-5 pb-16 sm:px-16 lg:px-28">
-      <div className="mt-10" />
-      <Header />
-      <div className="mt-16" />
-      <div>
-        <p className="mb-16 text-center text-3xl">게시판</p>
-
-        <StickyHeadTable />
-      </div>
-    </div>
-  );
+  redirect("/login");
+  return null;
 }

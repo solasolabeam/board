20b12d8ff269b6c4d7c6660582d73d01a1c7c53f
solar <nathf1005@naver.com>
2025-02-16 22:19:43 +0900

    feat: 헤더에 사용자 이름 표시 및 로그인 페이지에서 사용자 설정 기능 추가

--- src/app/(components)/Header.tsx
+++ src/app/(components)/Header.tsx
@@ -5,8 +5,8 @@ import useUserStore from "../store";
 
 export default function Header() {
   const router = useRouter();
+  const user = useUserStore((state) => state.user);
   const handleLogout = () => {
-    useUserStore.getState().setUser(null);
     document.cookie = "accessToken=; path=/; max-age=0;";
     document.cookie = "refreshToken=; path=/; max-age=0;";
     router.push("/login");
@@ -15,6 +15,7 @@ export default function Header() {
   return (
     <div className="mx-5">
       <div className="flex justify-end gap-5">
+        <p className="text-sm">{user?.username} 님</p> |
         <p className="cursor-pointer text-sm" onClick={handleLogout}>
           로그아웃
         </p>
